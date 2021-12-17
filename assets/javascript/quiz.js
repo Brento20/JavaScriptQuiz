3fddf326adf50b20b0fc5cfefcbb7b4d18f4846b
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-18 00:45:01 +1100

    failed to get data from arrazy and use it to fill my question field

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,6 +1,6 @@
 
 
-//things i need to link to HTML
+//Things i need to link to HTML
     // Timer start/stop
         var timerCount = document.querySelector("timerBox")
     // Questions
@@ -11,6 +11,22 @@
         var answerC = document.querySelector("answerC")
         var answerD = document.querySelector("answerD")
     // scoreboard
+        // Check for data in local storage
+
+
+// Storage for questions and answers
+const question1 = ["When watching Jumanji and writing JavaScript you must:", "Turn off the movie", "Google the IMDB of the whole cast and feel old", "Build as many Jumanji puns into your JS as possible", "Cry and accept defeat"];
+const question2 = [];
+const question3 = [];
+
+//TEST ZONE//
+function fillQuestion() {
+    questionText.textContent = question1[0];
+    answerA.textContent = question1[1];
+    answerB.textContent = question1[2];
+    answerC.textContent = question1[3];
+    answerD.textContent = question1[4];
+}
 
 
 // Storage for vars
