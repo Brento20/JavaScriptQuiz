90f5e58dd65a78e946e720532c0ec9abb44ded8c
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-17 23:31:38 +1100

    layout for HTML started and filee structure built

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,13 @@
+
+
+//things i need to link to HTML
+    // Timer start/stop
+        var timerCount = document.querySelector("timerCount")
+    // Questions
+        var questionText = document.querySelector("questionText")
+    // Answers and correct answers
+        var answerA = document.querySelector("answerA")
+        var answerB = document.querySelector("answerB")
+        var answerC = document.querySelector("answerC")
+        var answerD = document.querySelector("answerD")
+    // scoreboard
