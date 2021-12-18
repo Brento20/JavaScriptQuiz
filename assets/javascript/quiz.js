6a4efb7b1800fb806a82e40d3a616a64ba435318
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-18 18:14:35 +1100

    Had a win and worked out how to access the right information inside arrays. have hit a snag usinging innerHTML to set the value to the HTML at the moment though so thats fun

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -2,45 +2,65 @@
 
 //Things i need to link to HTML
     // Timer start/stop
-        var timerCount = document.querySelector("timerBox")
+        var timerCount = document.getElementById("timerBox")
     // Questions
-        var questionText = document.querySelector("questionText")
+        var questionText = document.getElementById("questionText")
     // Answers and correct answers
-        var optionA = document.querySelector("answerA")
-        var optionB = document.querySelector("answerB")
-        var optionC = document.querySelector("answerC")
-        var optionD = document.querySelector("answerD")
+        var optionA = document.getElementById("answerA")
+        var optionB = document.getElementById("answerB")
+        var optionC = document.getElementById("answerC")
+        var optionD = document.getElementById("answerD")
     // scoreboard
         // Check for data in local storage
         var highScore = localStorage.getItem('highScore');
 
 
 // Storage for questions and answers
-const questionContent = [ {
-    question: "When watching Jumanji and writing JavaScript you must:",
+const questionContent = [ 
+{   question: "When watching Jumanji and writing JavaScript you must:",
     optionA: "Turn off the movie",
     optionB: "Google the IMDB of the whole cast and feel old",
     optionC: "Build as many Jumanji puns into your JS as possible",
     optionD: "Cry and accept defeat",
     answer: "Build as many Jumanji puns into your JS as possible",
 },
-{   question: "Rolling the dice is to Jumanji, what ____________ is to 'Javamanji':",
-    choices: ["Using a boolean.", "Using math.random.", "Throwing your laptop into a river", "Calling the customer service number"],
+{   question: "Rolling the dice is to Jumanji, what ____________ is to 'Java-manji':",
+    optionA: "Using a boolean.",
+    optionB: "Throwing your laptop into a river",
+    optionC: "Calling the customer service number",
+    optionD: "Using math.random.",
     answer: "Using math.random.",
 }];
 
-console.log(questionContent);
+// GAME FUNCTION
 
+var lastQuestion = questionContent.length-1;
+var currentQuestion = 0;
+
+function displayQuestion(){
+    var question = questionContent[currentQuestion];
+    questionText.innerHTML = "<h1>" + question.question + "</h1>";
+    optionA.innerHTML = "<p>" + question.optionA + "</p>";
+    optionB.innerHTML = "<p>" + question.optionB + "</p>";
+    optionC.innerHTML = "<p>" + question.optionC + "</p>";
+    optionD.innerHTML = "<p>" + question.optionD + "</p>";
+
+}
+displayQuestion();
 
 //TEST ZONE//
-function fillQuestion(i) {
-    for (i=0 ; i<questionContent.length; i++){
-        let (questionText[i]) = questionContent.find(questionContent => questionContent.question);
-        console.log(questionText[i]);
+console.log(questionContent);
+console.log(questionContent[0].question)
 
-    }
-};
-fillQuestion();
+// function fillQuestion(i) {
+//     for (i=0 ; i<questionContent.length; i++){
+//         let (questionText[i]) = questionContent.find(questionContent => questionContent.question);
+//         console.log(questionText[i]);
+
+//     }
+// };
+// fillQuestion();
+//END TEST ZONE
 
 
 
