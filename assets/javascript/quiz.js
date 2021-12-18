b2a56530ab42db174432fbc5238f67928e44c60b
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-18 22:54:51 +1100

    started to add some event listeners, at first to make the startgame button dissapear and make the first question appear

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -3,18 +3,37 @@
 //Things i need to link to HTML
     // Timer start/stop
         var timerCount = document.getElementById("timerBox")
-    // Questions
+    // Start Game button
+        var startButton = document.getElementById("start")
+    // Questions and Options ID's linking to HTML
         var questionText = document.getElementById("questionText")
-    // Answers and options
         var optionA = document.getElementById("optionA")
         var optionB = document.getElementById("optionB")
         var optionC = document.getElementById("optionC")
         var optionD = document.getElementById("optionD")
+    // Event Listeners
+    startButton.addEventListener("click", startGame)
+    // optionA.addEventListener("click", );
+
+
     // scoreboard
         // Check for data in local storage
         var highScore = localStorage.getItem('highScore');
         var scores = 0;
         timerInitialLength = 60;
+
+
+// START GAME FUNCTION
+
+    function startGame (){
+        startButton.style.display = "none";
+        questionText.style.display = "block";
+        optionA.style.display = "block";
+        optionB.style.display = "block";
+        optionC.style.display = "block";
+        optionD.style.display = "block";
+        //start timer here
+    }
 
 
 // Storage for questions and answers
@@ -24,22 +43,36 @@ const questionContent = [
     optionB: "Google the IMDB of the whole cast and feel old",
     optionC: "Build as many Jumanji puns into your JS as possible",
     optionD: "Cry and accept defeat",
-    answer: "Build as many Jumanji puns into your JS as possible",
+    answer: "optionC",
 },
 {   question: "Rolling the dice is to Jumanji, what ____________ is to 'Java-manji':",
     optionA: "Using a boolean.",
     optionB: "Throwing your laptop into a river",
     optionC: "Calling the customer service number",
     optionD: "Using math.random.",
-    answer: "Using math.random.",
+    answer: "optionD.",
+},
+{   question: "After adding a click event listener, the user can...",
+    optionA: "Hear a clicking sound in the walls",
+    optionB: "Expect the application to respond to mouse clicks",
+    optionC: "Use the mouse like a throwing star",
+    optionD: "Roll the dice, landing on pairs 3 times, to exit the game",
+    answer: "optionB",
+},
+{   question: "The chance of receiving additional points for having so many Jumanji references in a JavaScript quiz is?",
+    optionA: "Extremely high, the content was very entertaining",
+    optionB: "Low, I hate Jumanji",
+    optionC: "The application quit unexpectedly, report issue?",
+    optionD: "Ive been grading for hours and I just wanna take a nap?",
+    answer: "optionA",
 }];
 
 // GAME FUNCTION
 
-var lastQuestion = questionContent.length-1;
-var currentQuestion = 0;
+var lastQuestion = questionContent.length-1; // I need to know this so I can end the game
+var currentQuestion = 0; // This is set to 0 because my questions are objects in the array above, I want to be able to ++ the currentQuestion value to move on to the next question as we go along.
 
-function displayQuestion(){
+function displayQuestion(){ //im pulling data out of my questionContent array and using innerHTML to fill in the ol/li in my index.html using the ID tags i pulled using getElementId.
     var question = questionContent[currentQuestion];
     questionText.innerHTML = "<h2>" + question.question + "</h2>";
     optionA.innerHTML = question.optionA;
@@ -47,19 +80,22 @@ function displayQuestion(){
     optionC.innerHTML = question.optionC;
     optionD.innerHTML = question.optionD;
     
+}
 
-        function checkAnswers(userAnswer) {
-            if (question[currentQuestion].answer == userAnswer){
-                scores++;
-                currentQuestion++;
-            }else {
-                timerInitialLength-10;
-                currentQuestion++;
-        }
+function checkAnswers(userAnswer) {
+    if (questionContent[currentQuestion].answer == userAnswer){
+        scores++;
+        currentQuestion++;
+    }else {
+        timerInitialLength-10;
+        currentQuestion++;
     }
 }
 
+
 displayQuestion();
+checkAnswers();
+
 
 
 
