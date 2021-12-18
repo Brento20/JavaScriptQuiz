b46f4856172973c3f752930e63ea0f6d9f7e2d43
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-18 13:37:11 +1100

    added to the questions field becuse its the only part that my brain can handle rn

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -15,15 +15,15 @@
 
 
 // Storage for questions and answers
-const questionContent = {
+const questionContent = [ {
     question: "When watching Jumanji and writing JavaScript you must:",
     choices: ["Turn off the movie", "Google the IMDB of the whole cast and feel old", "Build as many Jumanji puns into your JS as possible", "Cry and accept defeat"],
     answer: "Build as many Jumanji puns into your JS as possible",
-};
-
-
-
-
+},
+{   question: "Rolling the dice is to Jumanji, what ____________ is to 'Javamanji':",
+    choices: ["Using a boolean.", "Using math.random.", "Throwing your laptop into a river", "Calling the customer service number"],
+    answer: "Using math.random.",
+}];
 
 
 //TEST ZONE//
